8d7ed6691ff9a530c560db77a6288310b8ea87c6
ismailgungor <ismail.gungor.92@gmail.com>
2018-01-24 18:22:16 +0300

    Changed return type to ArrayList<Event> for getAll and findByName methods in EventsSql

--- rateme/src/main/java/com/zingat/rateme/EventsSql.kt
+++ rateme/src/main/java/com/zingat/rateme/EventsSql.kt
@@ -1,6 +1,7 @@
 package com.zingat.rateme
 
 import android.content.Context
+import com.zingat.rateme.model.Event
 
 /**
  * Created by ismailgungor on 24.01.2018.
@@ -9,18 +10,18 @@ class EventsSql(context: Context) {
 
     private val dbEvents: EventsDb = EventsDb(context, EventsDb.DATABASE_NAME, null, EventsDb.DATABASE_VERSION)
 
-    fun findByName(eventName: String): ArrayList<String> {
+    fun findByName(eventName: String): ArrayList<Event> {
 
-        return ArrayList<String>()
+        return ArrayList<Event>()
     }
 
     fun save(event: String) {
 
     }
 
-    fun getAll(): ArrayList<String> {
+    fun getAll(): ArrayList<Event> {
 
-        return ArrayList<String>()
+        return ArrayList<Event>()
     }
 
 
