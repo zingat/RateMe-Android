71b3fa8eb5d569c111ad5eeee1187ccaae1ad990
ismailgungor <ismail.gungor.92@gmail.com>
2018-01-24 15:25:16 +0300

    Created CheckCondition class, initialize "isConditionComplete" and "isReminderEnd" methods

---
+++ rateme/src/main/java/com/zingat/rateme/CheckCondition.kt
@@ -0,0 +1,16 @@
+package com.zingat.rateme
+
+/**
+ * Created by ismailgungor on 24.01.2018.
+ */
+class CheckCondition {
+
+    fun isConditionsComplete(conditionList: ArrayList<Condition>, nameList: ArrayList<String>): Boolean {
+
+        return false
+    }
+
+    fun isReminderEnd(): Boolean {
+        return false
+    }
+}
