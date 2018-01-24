9145cd3ae6615fe29d27345b07c6c4e1e3ba7ce5
olkunmustafa <olkun.mustafa@gmail.com>
2018-01-24 13:43:41 +0300

    Kotlin configure was made for every package.

--- app/src/main/java/com/zingat/ratemesample/MainActivity.kt
+++ app/src/main/java/com/zingat/ratemesample/MainActivity.kt
@@ -2,11 +2,17 @@ package com.zingat.ratemesample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.zingat.rateme.Rateme
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val rateme : Rateme = Rateme()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView.text = rateme.test()
     }
 }
---
+++ rateme/src/main/java/com/zingat/rateme/Rateme.kt
@@ -0,0 +1,12 @@
+package com.zingat.rateme
+
+/**
+ * Created by mustafaolkun on 24/01/2018.
+ */
+class Rateme {
+
+    fun test() : String{
+        return "Hello world"
+    }
+
+}
