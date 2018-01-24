75a640f053344575ca2bab3a177d005524abb70f
ismailgungor <ismail.gungor.92@gmail.com>
2018-01-24 15:28:59 +0300

    Created DataHelper class, initialize "saveEvent", "getAllEvents", "findByEventName" and "getReminder" methods

---
+++ rateme/src/main/java/com/zingat/rateme/DataHelper.kt
@@ -0,0 +1,27 @@
+package com.zingat.rateme
+
+/**
+ * Created by ismailgungor on 24.01.2018.
+ */
+class DataHelper {
+
+    fun saveEvent(event: String) {
+
+    }
+
+    fun getAllEvents(): ArrayList<String> {
+
+        return ArrayList<String>()
+    }
+
+    fun findByEventName(event: String): ArrayList<String> {
+
+        return ArrayList<String>()
+    }
+
+    fun getReminder(): Long {
+
+        return 0L
+    }
+
+}
