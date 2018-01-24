15730203fe83b381480ee3b9a9580927349c2866
ismailgungor <ismail.gungor.92@gmail.com>
2018-01-24 15:07:41 +0300

    Created Condition class and singleton object for Readme class

---
+++ rateme/src/main/java/com/zingat/rateme/Condition.kt
@@ -0,0 +1,28 @@
+package com.zingat.rateme
+
+/**
+ * Created by ismailgungor on 24.01.2018.
+ */
+class Condition(count: Int, type: String) {
+
+    private var mCount = count
+    private var mType = type
+
+    fun setCount(count: Int) {
+        this.mCount = count
+    }
+
+    fun getCount(): Int {
+
+        return this.mCount
+    }
+
+    fun setType(type: String) {
+        this.mType = type
+    }
+
+    fun getType(): String {
+        return this.mType
+    }
+
+}
--- rateme/src/main/java/com/zingat/rateme/Rateme.kt
+++ rateme/src/main/java/com/zingat/rateme/Rateme.kt
@@ -1,12 +1,35 @@
 package com.zingat.rateme
 
+import android.content.Context
+
 /**
  * Created by mustafaolkun on 24/01/2018.
  */
-class Rateme {
+class Rateme() {
 
-    fun test() : String{
-        return "Hello world"
+    lateinit var mContext: Context
+    lateinit var mConditionList: ArrayList<Condition>
+
+    companion object {
+
+        private val mRateme: Rateme by lazy { Rateme() }
+
+        fun getInstance(): Rateme {
+
+            return mRateme
+        }
+
     }
+
+    fun setContext(context: Context): Rateme {
+        this.mContext = context
+        return this
+    }
+
+    fun setConditionList(conditionList: ArrayList<Condition>): Rateme {
+        this.mConditionList = conditionList
+        return this
+    }
+
 
 }
