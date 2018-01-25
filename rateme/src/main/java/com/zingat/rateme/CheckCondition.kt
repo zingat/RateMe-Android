15fd358af3ee4e8eeaf32767ce0278b658723293
ismailgungor <ismail.gungor.92@gmail.com>
2018-01-25 14:35:26 +0300

    Modified in order to fill "isConditionsComplete" and "isReminderEnd" methods

--- rateme/src/main/java/com/zingat/rateme/CheckCondition.kt
+++ rateme/src/main/java/com/zingat/rateme/CheckCondition.kt
@@ -1,18 +1,56 @@
 package com.zingat.rateme
 
 import com.zingat.rateme.model.Condition
+import com.zingat.rateme.model.Event
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by ismailgungor on 24.01.2018.
  */
 class CheckCondition {
 
-    fun isConditionsComplete(conditionList: ArrayList<Condition>, nameList: ArrayList<String>): Boolean {
+    fun isConditionsComplete(conditionList: ArrayList<Condition>, eventList: ArrayList<Event>): Boolean {
+
+        val currentEventMap = HashMap<String, Int>()
+
+        eventList
+                .map { it.getName() }
+                .forEach {
+                    if (currentEventMap.containsKey(it)) {
+                        val currentValue = currentEventMap[it]!!
+
+                        currentEventMap[it] = currentValue + 1
+                    } else {
+                        currentEventMap[it] = 1
+                    }
+                }
+
+
+        for (condition in conditionList) {
+
+            val currentConditionType = condition.getType()
+            val currentConditionValue = condition.getCount()
+
+            if (currentEventMap.containsKey(currentConditionType) && currentEventMap[currentConditionType] == currentConditionValue) {
+                return true
+            }
+        }
 
         return false
     }
 
-    fun isReminderEnd(): Boolean {
+    fun isReminderEnd(reminderValue: Long, reminderDuration: Int): Boolean {
+
+        val currentTime = Calendar.getInstance().timeInMillis
+
+        val diffInMilis = currentTime - reminderValue
+        val diffInDays = TimeUnit.MILLISECONDS.toDays(diffInMilis)
+
+        if (diffInDays > reminderDuration) {
+            return true
+        }
+
         return false
     }
 }
