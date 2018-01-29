ed0d7a868552cf3bb42b05dfd6f1fa6f059a2974
olkunmustafa <olkun.mustafa@gmail.com>
2018-01-29 12:38:36 +0300

    Test class were create for Rateme.kt

---
+++ rateme/src/test/java/com/zingat/rateme/RatemeTest.kt
@@ -0,0 +1,26 @@
+package com.zingat.rateme
+
+import org.assertj.core.api.Assertions
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import org.mockito.Spy
+
+
+/**
+ * Created by mustafaolkun on 26/01/2018.
+ */
+class RatemeTest {
+
+    lateinit var rateMe: Rateme
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+
+    }
+
+
+}
