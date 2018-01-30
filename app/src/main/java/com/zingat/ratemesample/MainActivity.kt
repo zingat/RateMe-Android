e709f8443cba8f31169ed01f1fa0ef9edb4ebff6
ismailgungor <ismail.gungor.92@gmail.com>
2018-01-30 11:37:39 +0300

    Applied Rateme library changes in MainActivity

--- app/src/main/java/com/zingat/ratemesample/MainActivity.kt
+++ app/src/main/java/com/zingat/ratemesample/MainActivity.kt
@@ -2,7 +2,6 @@ package com.zingat.ratemesample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.Telephony
 import android.view.View
 import android.widget.Toast
 import com.zingat.rateme.Rateme
@@ -20,9 +19,9 @@ class MainActivity : AppCompatActivity() {
         this.mRateme = Rateme.getInstance()
 
         mRateme.setContext(this)
-                .addCondition("buton", 3)
+                .addCondition("button", 3)
                 .reminderDuration(0)
-                .createDialogWithCustomView(R.layout.layout_dialog)
+                .create(R.layout.layout_dialog)
                 .initCustomDialogButtons(R.drawable.rate_button_background,
                         R.drawable.later_button_background,
                         R.drawable.never_button_background)
@@ -34,13 +33,7 @@ class MainActivity : AppCompatActivity() {
 
         count += 1
 
-        if (mRateme.isRatemeEnable()) {
-
-            mRateme.addEvent("buton")
-            mRateme.startShowProcess()
-
-        }
-
+        mRateme.addEvent("button")
 
         Toast.makeText(this, "$count kere dokunuldu", Toast.LENGTH_SHORT).show()
 
